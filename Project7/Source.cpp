1560810b721fedf1be70e2cd7b34c49a30ae725f
Alsy <alsy1311@yandex.ru>
2020-03-15 17:12:15 +0300

    перемешала П1 случайным образом

--- Project7/Source.cpp
+++ Project7/Source.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <random>
 int main()
 {
 	std::vector<int> v{ 1,2,3,4,5,6,7,8,9,10 };
@@ -10,10 +11,17 @@ int main()
 		std::cin >> l;
 		std::fill_n(std::back_inserter(v), 1, l);
 	}
-	for (int i = 0; i < 13; i++)
+	/*for (int i = 0; i < 13; i++)
 	{
 		std::cout << v[i] << std::endl;
 	}
-
+	*/
+	std::random_device rd;
+	std::mt19937 g(rd());
+	std::shuffle(v.begin(), v.end(), g);
+	for (int i = 0; i < v.size(); i++)
+	{
+		std::cout << v[i] << std::endl;
+	}
 	system("pause");
 }
