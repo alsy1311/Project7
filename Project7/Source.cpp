8ecf0f37cb89f99fcc0d8222d09b582d5cc05a09
Alsy <alsy1311@yandex.ru>
2020-03-15 19:57:29 +0300

    удалила дубликаты

--- Project7/Source.cpp
+++ Project7/Source.cpp
@@ -5,23 +5,27 @@
 #include <random>
 int main()
 {
+
 	std::vector<int> v{ 1,2,3,4,5,6,7,8,9,10 };
 	for (int t = 0; t < 3; t++) {
 		int l = 0;
 		std::cin >> l;
 		std::fill_n(std::back_inserter(v), 1, l);
 	}
-	/*for (int i = 0; i < 13; i++)
-	{
-		std::cout << v[i] << std::endl;
-	}
-	*/
+
 	std::random_device rd;
 	std::mt19937 g(rd());
 	std::shuffle(v.begin(), v.end(), g);
-	for (int i = 0; i < v.size(); i++)
-	{
-		std::cout << v[i] << std::endl;
-	}
-	system("pause");
+	std::sort(v.begin(), v.end());
+	auto last = std::unique(v.begin(), v.end());
+	v.erase(last, v.end());
+		for (int i = 0; i < v.size(); i++)
+		{
+			std::cout << v[i] << std::endl;
+		}
+
+		system("pause");
+
 }
+
+
