303ee4be84db6e21af04ddc0a46dfd852995dbc4
Alsy <alsy1311@yandex.ru>
2020-03-15 16:56:28 +0300

    добавила 3 элемента в конец  cin

--- Project7/Source.cpp
+++ Project7/Source.cpp
@@ -5,4 +5,15 @@
 int main()
 {
 	std::vector<int> v{ 1,2,3,4,5,6,7,8,9,10 };
+	for (int t = 0; t < 3; t++) {
+		int l = 0;
+		std::cin >> l;
+		std::fill_n(std::back_inserter(v), 1, l);
+	}
+	for (int i = 0; i < 13; i++)
+	{
+		std::cout << v[i] << std::endl;
+	}
+
+	system("pause");
 }
